a5434a6f5eca2d5e02bb4affe096f479ef356caa
felipe.conceicao <felipeconceicao452@gmail.com>
2026-09-01 17:43:01 -0300

    added .env.example and fixed bugs

--- src/pages/TakeTicket.jsx
+++ src/pages/TakeTicket.jsx
@@ -1,19 +1,11 @@
-import { useEffect } from "react";
 import MenuSenha from "../components/MenuSenha";
-import { ultimaSenhasChamadas } from "../services/senhaService";
 function TakeTicket() {
-    useEffect(() => {
-        ultimaSenhasChamadas();
-    }, [])
     return (
         <div className="flex flex-col h-full p-6 w-full text-black">
             <h1 className="text-4xl font-bold ml-3">Retire sua <span className='text-primary'>senha</span></h1>
             <p className="mt-2 mb-4 ml-3 text-gray-500">Selecione o tipo de atendimento <br/> para continuar</p>
   
             <MenuSenha />
-            {/* <div className="mt-2">
-                <Button name="Voltar" variant="filled" to="/"/>
-            </div> */}
         </div>
     );
 }
